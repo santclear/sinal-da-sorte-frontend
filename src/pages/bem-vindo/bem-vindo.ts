0258f6f13387be0b74140d35e8b3853061404605
santclear <santclear@gmail.com>
2016-11-30 23:39:34 -0200

    Modelando view Bem Vindo.

--- src/pages/bem-vindo/bem-vindo.ts
+++ src/pages/bem-vindo/bem-vindo.ts
@@ -7,11 +7,18 @@ import {PaginaBase} from '../pagina.base';
     templateUrl: 'bem-vindo.html'
 })
 export class BemVindoPage extends PaginaBase {
+	public dezenas;
 
     constructor(public nav: NavController, private menu: MenuController) {
         super();
         this.setTitulo("Bem Vindo");
 		
-        this.menu.open();
+        // this.menu.open();
+
+		this.dezenas = [
+			{numero: '01'},{numero: '02'},{numero: '03'},{numero: '04'},{numero: '05'},
+			{numero: '06'},{numero: '07'},{numero: '08'},{numero: '09'},{numero: '10'},
+			{numero: '11'},{numero: '12'},{numero: '13'},{numero: '14'},{numero: '15'},
+		];
     }
 }
