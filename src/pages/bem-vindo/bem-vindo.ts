8a36703f3edb335f82de29f3185625c5782da4b1
santclear <santclear@gmail.com>
2016-12-01 23:25:52 -0200

    Implementando a view Bem Vindo, adicionado novas características ao tema e atualizado versão do Bootstrap

--- src/pages/bem-vindo/bem-vindo.ts
+++ src/pages/bem-vindo/bem-vindo.ts
@@ -8,17 +8,23 @@ import {PaginaBase} from '../pagina.base';
 })
 export class BemVindoPage extends PaginaBase {
 	public dezenas;
+	public exibeDezenasComQuebraDeLinha: boolean;
 
     constructor(public nav: NavController, private menu: MenuController) {
         super();
         this.setTitulo("Bem Vindo");
 		
-        // this.menu.open();
+		this.exibeDezenasComQuebraDeLinha = true;
 
 		this.dezenas = [
 			{numero: '01'},{numero: '02'},{numero: '03'},{numero: '04'},{numero: '05'},
 			{numero: '06'},{numero: '07'},{numero: '08'},{numero: '09'},{numero: '10'},
 			{numero: '11'},{numero: '12'},{numero: '13'},{numero: '14'},{numero: '15'},
 		];
+
+
+        // this.menu.open();
+
+		
     }
 }
