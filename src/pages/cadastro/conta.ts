be5c51afd89cfb9cdd3cee63809b92be540770df
Sant'Clear <santclear@gmail.com>
2018-02-24 18:32:38 -0300

    Criando um FormGroup para controlar o formulário de cadastro de conta

--- src/pages/cadastro/conta.ts
+++ src/pages/cadastro/conta.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'page-conta',
@@ -7,7 +8,28 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ContaPage {
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	formGroup: FormGroup;
+
+	constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+		this.formGroup = this.formBuilder.group({
+			nome: ['Elon', [Validators.required, Validators.minLength(3), Validators.maxLength(130)]],
+			sobrenome: ['Musk', [Validators.required, Validators.minLength(3), Validators.maxLength(130)]],
+			email: ['elon@gmail.com', [Validators.required, Validators.email]],
+			generoId: [1, [Validators.required]],
+			dataDeNascimento: ['1', [Validators.required, Validators.pattern('dd/dd/dddd')]],
+			cpf: ['06134596280', [Validators.required, Validators.pattern('ddd.ddd.ddd-ddd')]],
+			senha: ['123', [Validators.required]],
+			logradouro: ['Rua Via', [Validators.required]],
+			numero: ['25', [Validators.required]],
+			complemento: ['Apto 3', []],
+			bairro: ['Copacabana', [Validators.required]],
+			cep: ['10828333', [Validators.required, Validators.pattern('ddddd-ddd')]],
+			telefone1: ['977261827', [Validators.required]],
+			telefone2: ['', [Validators.minLength(11), Validators.maxLength(30)]],
+			telefone3: ['', [Validators.minLength(11), Validators.maxLength(30)]],
+			estadoId: [null, [Validators.required]],
+			cidadeId: [null, [Validators.required]]
+		});
 	}
 
 	cadastre() {
