5ac04866ee136269a5ddb98d09e0251127648378
Sant'Clear <santclear@gmail.com>
2018-04-04 01:25:51 -0300

    Ready only nos itens de menu que tem a respectiva funcionalidade desenvolvida.

--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -31,12 +31,12 @@ export class MenuService {
 		return [
 			{ sufixoCssPagina: 'Resultado', titulo: 'Resultados', class: 'ResultadoPage', icone: 'done-all', corTexto: cor, exibir_texto: 'none' },
 			{ sufixoCssPagina: 'Estatistica', titulo: 'Estatística', class: 'EstatisticaPage', icone: 'trending-up', corTexto: cor, exibir_texto: 'none' },
-			{ sufixoCssPagina: 'Simulador', titulo: 'Simulador', class: 'SimuladorPage', icone: 'car', corTexto: '#bbb', exibir_texto: 'inline' },
-			{ sufixoCssPagina: 'Fechamento', titulo: 'Fechamento', class: 'FechamentoPage', icone: 'lock', corTexto: '#bbb', exibir_texto: 'inline' },
-			{ sufixoCssPagina: 'Aposta', titulo: 'Aposta', class: 'ApostaPage', icone: 'cash', corTexto: '#bbb', exibir_texto: 'inline' },
-			{ sufixoCssPagina: 'GruposEspeciais', titulo: 'Grupos Especiais', class: 'GruposEspeciaisPage', icone: 'grid', corTexto: '#bbb', exibir_texto: 'inline' },
-			{ sufixoCssPagina: 'Bolao', titulo: 'Bolão', class: 'BolaoPage', icone: 'people', corTexto: '#bbb', exibir_texto: 'inline' },
-			{ sufixoCssPagina: 'HistoricoDeApostas', titulo: 'Histórico de Apostas', class: 'HistoricoDeApostasPage', icone: 'list-box', corTexto: '#bbb', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'Simulador', titulo: 'Simulador', class: 'SimuladorPage', icone: 'car', corTexto: '#777', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'Fechamento', titulo: 'Fechamento', class: 'FechamentoPage', icone: 'lock', corTexto: '#777', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'Aposta', titulo: 'Aposta', class: 'ApostaPage', icone: 'cash', corTexto: '#777', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'GruposEspeciais', titulo: 'Grupos Especiais', class: 'GruposEspeciaisPage', icone: 'grid', corTexto: '#777', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'Bolao', titulo: 'Bolão', class: 'BolaoPage', icone: 'people', corTexto: '#777', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'HistoricoDeApostas', titulo: 'Histórico de Apostas', class: 'HistoricoDeApostasPage', icone: 'list-box', corTexto: '#777', exibir_texto: 'inline' },
 			{ sufixoCssPagina: 'Conta', titulo: 'Conta', class: 'AtualizacaoContaPage', icone: 'key', corTexto: cor, exibir_texto: 'none' },
 			{ sufixoCssPagina: 'Sair', titulo: 'Sair', class: 'LoginPage', icone: 'exit', corTexto: cor, exibir_texto: 'none' }
 		];
