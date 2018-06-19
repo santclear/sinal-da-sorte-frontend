15f8ccaac083aa1f7a188badba38d5509d5bd2b6
Sant'Clear <santclear@gmail.com>
2018-06-19 00:09:51 -0300

    Ajustes no tema.

--- src/pages/estatistica/estatistica.ts
+++ src/pages/estatistica/estatistica.ts
@@ -10,15 +10,22 @@ import { IonicPage } from 'ionic-angular';
 })
 export class EstatisticaPage extends PaginaBase {
 	public cbxTipoDeGrafico: string = 'frequenciaAcumulada';
-	public tiposDeGrafico: {id: string, tipo: string}[];
+	public tiposDeGrafico: {id: string, tipo: string, desabilitado: boolean}[];
 
 	constructor() {
 		super();
         this.setTitulo('Estatística');
 
 		this.tiposDeGrafico = [
-			{id: 'frequenciaAcumulada', tipo: 'Acumulado'},
-			{id: 'frequenciaSomaDezenas', tipo: 'Soma dezenas'}
+			{id: 'frequenciaAcumulada', tipo: 'Frequência', desabilitado: false},
+			{id: 'frequenciaSomaDezenas', tipo: 'Soma dezenas', desabilitado: false},
+			{id: 'a', tipo: 'Dezenas por Casa', desabilitado: true},
+			{id: 'a', tipo: 'Dezenas Repetidas', desabilitado: true},
+			{id: 'a', tipo: 'Linhas', desabilitado: true},
+			{id: 'a', tipo: 'Colunas', desabilitado: true},
+			{id: 'a', tipo: 'Quadrantes', desabilitado: true},
+			{id: 'a', tipo: 'Pares e Ímpares', desabilitado: true},
+			{id: 'a', tipo: 'Primos', desabilitado: true},
 		];
 
 		let bd = ConexaoFabrica.getConexao();
--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -36,10 +36,22 @@ export class MenuService {
 			{ sufixoCssPagina: 'Sair', titulo: 'Sair', class: 'LoginPage', icone: 'exit', corTexto: cor, exibir_texto: 'none' },
 			{ sufixoCssPagina: 'Simulador', titulo: 'Simulador', class: 'SimuladorPage', icone: 'game-controller-b', corTexto: '#777', exibir_texto: 'inline' },
 			{ sufixoCssPagina: 'Fechamento', titulo: 'Fechamento', class: 'FechamentoPage', icone: 'lock', corTexto: '#777', exibir_texto: 'inline' },
-			{ sufixoCssPagina: 'Aposta', titulo: 'Volante', class: 'ApostaPage', icone: 'document', corTexto: '#777', exibir_texto: 'inline' },
-			// { sufixoCssPagina: 'GruposEspeciais', titulo: 'Grupos Especiais', class: 'GruposEspeciaisPage', icone: 'grid', corTexto: '#777', exibir_texto: 'inline' },
-			{ sufixoCssPagina: 'Bolao', titulo: 'Bolão', class: 'BolaoPage', icone: 'people', corTexto: '#777', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'Aposta', titulo: 'Geração de Volantes', class: 'ApostaPage', icone: 'document', corTexto: '#777', exibir_texto: 'inline' },
 			{ sufixoCssPagina: 'HistoricoDeApostas', titulo: 'Histórico de Volantes', class: 'HistoricoDeApostasPage', icone: 'list-box', corTexto: '#777', exibir_texto: 'inline' },
+			{ sufixoCssPagina: 'Bolao', titulo: 'Bolão', class: 'BolaoPage', icone: 'people', corTexto: '#777', exibir_texto: 'inline' },
+			// // { sufixoCssPagina: 'GruposEspeciais', titulo: 'Grupos Especiais', class: 'GruposEspeciaisPage', icone: 'grid', corTexto: '#777', exibir_texto: 'inline' },
+			
+			
+			// { sufixoCssPagina: 'Resultado', titulo: 'Resultados', class: 'ResultadoPage', icone: 'done-all', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Estatistica', titulo: 'Estatística', class: 'EstatisticaPage', icone: 'trending-up', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Simulador', titulo: 'Simulador', class: 'SimuladorPage', icone: 'game-controller-b', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Fechamento', titulo: 'Fechamento', class: 'FechamentoPage', icone: 'lock', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Aposta', titulo: 'Geração de Volantes ', class: 'ApostaPage', icone: 'document', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'HistoricoDeApostas', titulo: 'Histórico de Volantes', class: 'HistoricoDeApostasPage', icone: 'list-box', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Bolao', titulo: 'Bolão', class: 'BolaoPage', icone: 'people', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Conta', titulo: 'Conta', class: 'AtualizacaoContaPage', icone: 'key', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Contato', titulo: 'Contato', class: 'ContatoPage', icone: 'contact', corTexto: cor, exibir_texto: 'none' },
+			// { sufixoCssPagina: 'Sair', titulo: 'Sair', class: 'LoginPage', icone: 'exit', corTexto: cor, exibir_texto: 'none' },
 		];
 	}
 	
