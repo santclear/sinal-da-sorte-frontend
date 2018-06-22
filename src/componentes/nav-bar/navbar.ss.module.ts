cebf23d189d630bc0a817c4ecbafbe204ce80e57
Sant'Clear <santclear@gmail.com>
2018-06-21 23:59:54 -0300

    Adequado componente NavBar.

--- src/componentes/nav-bar/navbar.ss.module.ts
+++ src/componentes/nav-bar/navbar.ss.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavBarSs } from './navbar.ss';
 import { IonicModule } from 'ionic-angular';
 
 @NgModule({
 	declarations: [ NavBarSs ],
 	imports: [ IonicModule ],
-	exports: [ NavBarSs ]
+	exports: [ NavBarSs ],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NavBarSsModule { }
