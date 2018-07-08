3fcab9372424dde1e500633194f973c50fe7d5ca
Sant'Clear <santclear@gmail.com>
2018-07-08 02:20:20 -0300

    Ajustes incluíndo pages faltantes para se adequar à regra 'Usuário não logado é redirecionado para a page ContaExterno quando tenta navegar via URL nas pages internas'.

--- src/pages/simulador/simulador.ts
+++ src/pages/simulador/simulador.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { PaginaBase } from '../pagina.base';
+import { StorageService } from '../../services/storage.service';
+import { ContaLocalDTO } from '../../dtos/conta-local.dto';
 
 @IonicPage()
 @Component({
@@ -9,8 +11,13 @@ import { PaginaBase } from '../pagina.base';
 })
 export class SimuladorPage extends PaginaBase {
 
-    constructor(public nav: NavController) {
+    constructor(public navCtrl: NavController, public storage: StorageService) {
         super();
         this.setTitulo("Simulador");
     }
+
+    ionViewDidEnter() {
+		let contaLocal: ContaLocalDTO = this.storage.getContaLocal();
+		if(!contaLocal) this.navCtrl.setRoot('ContaExternoPage');
+	}
 }
