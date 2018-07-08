83af2903a87d0e39ee4f1d86352bbc273bb68702
Sant'Clear <santclear@gmail.com>
2018-07-08 02:10:17 -0300

    Ajustes no navbar externo.

--- src/componentes/navbar-conversao-landscape/navbar-conversao-landscape.ss.ts
+++ src/componentes/navbar-conversao-landscape/navbar-conversao-landscape.ss.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
 	selector: "ss-navbar-conversao-landscape",
 	templateUrl: 'navbar-conversao-landscape.ss.html'
 })
 export class NavbarConversaoLandscapeSs {
-	constructor() {}
+	constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+	abraContato() {
+		this.navCtrl.setRoot('ContatoExternoPage');
+	}
+
+	abraConta() {
+		this.navCtrl.setRoot('ContaExternoPage');
+	}
+
+	abraLogin() {
+		this.navCtrl.setRoot('LoginPage');
+	}
+
+	goLanding() {
+		this.navCtrl.setRoot('LandingPage');
+	}
 }
