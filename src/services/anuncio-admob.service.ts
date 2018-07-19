fa559f75ed9bbc450b39e84116eebae18d7272b5
Sant'Clear <santclear@gmail.com>
2018-07-18 22:43:14 -0300

    Excluído anúncio interstial.

--- src/services/anuncio-admob.service.ts
+++ src/services/anuncio-admob.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Platform } from 'ionic-angular';
-import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from "@ionic-native/admob-free";
+import { AdMobFree, AdMobFreeBannerConfig} from "@ionic-native/admob-free";
 
 @Injectable()
 export class AnuncioAdMobService {
@@ -8,12 +8,11 @@ export class AnuncioAdMobService {
 	constructor(public admob: AdMobFree, public plataforma: Platform) {
 		if(this.plataforma.is('android')) {
 			this.mostreAnuncioBanner();
-			this.mostreAnuncioInterstitial();
+			// this.mostreAnuncioInterstitial();
 		}
 	}
 
-	mostreAnuncioBanner() {
- 
+	mostreAnuncioBanner() { 
         let bannerConfig: AdMobFreeBannerConfig = {
             // isTesting: true, // Remove in production
             autoShow: true,
@@ -23,25 +22,23 @@ export class AnuncioAdMobService {
         this.admob.banner.config(bannerConfig);
  
         this.admob.banner.prepare().then(() => {
-            console.log('banner')
         }).catch(e => console.log(e));
- 
 	}
 	
-    mostreAnuncioInterstitial() {
-		let interstitialConfig: AdMobFreeInterstitialConfig = {
-			// isTesting: true, // Remove in production
-			autoShow: true,
-			id: 'ca-app-pub-5335868077868255/2236896705'
-		};
+    // mostreAnuncioInterstitial() {
+	// 	let interstitialConfig: AdMobFreeInterstitialConfig = {
+	// 		// isTesting: true, // Remove in production
+	// 		autoShow: true,
+	// 		id: 'ca-app-pub-5335868077868255/2236896705'
+	// 	};
 	
-		this.admob.interstitial.config(interstitialConfig);
+	// 	this.admob.interstitial.config(interstitialConfig);
 	
-		this.admob.interstitial.prepare().then(() => {
-		}).catch(e => {console.log(e)});
+	// 	this.admob.interstitial.prepare().then(() => {
+	// 	}).catch(e => {console.log(e)});
 
-		setTimeout(() => {
-			this.mostreAnuncioInterstitial();
-		}, 120000);
-    }
+	// 	setTimeout(() => {
+	// 		this.mostreAnuncioInterstitial();
+	// 	}, 120000);
+    // }
 }
