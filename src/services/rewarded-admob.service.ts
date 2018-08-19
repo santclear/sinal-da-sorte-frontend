9cd14596a6a58906308acf41ba900b660d99d695
Sant'Clear <santclear@gmail.com>
2018-08-18 21:39:18 -0300

    Excluído anúncio premiado.

--- src/services/rewarded-admob.service.ts
+++ src/services/rewarded-admob.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { Platform, ToastController, AlertController } from 'ionic-angular';
-import { AdMobFree, AdMobFreeRewardVideoConfig} from "@ionic-native/admob-free";
+import { /* Platform,  */ToastController, AlertController } from 'ionic-angular';
+// import { AdMobFree, AdMobFreeRewardVideoConfig} from "@ionic-native/admob-free";
 import { MenuService } from "./menu.service";
 import { ConexaoFabrica } from "../dao/util/conexao-fabrica";
 import { Observable } from 'rxjs/Observable';
@@ -14,57 +14,58 @@ export class RewardVideoAdMobService {
 	public dismiss: any;
 
 
-	private exibirMensagem: boolean = true;
+	// private exibirMensagem: boolean = true;
 
 	constructor(
-		private admob: AdMobFree, 
+		// private admob: AdMobFree, 
 		private menuService: MenuService,
 		private toastCtrl: ToastController,
 		private alertCtrl: AlertController,
-		private plataforma: Platform) {
+		// private plataforma: Platform
+	) {
 
 		this.bd = ConexaoFabrica.getConexao();
 		
-		if(this.plataforma.is('android')) {
-			document.addEventListener('admob.rewardvideo.events.REWARD', () => {
-				this.exibirMensagem = false;
-				this.sincronizeResultados();
-			});
+		// if(this.plataforma.is('android')) {
+		// 	document.addEventListener('admob.rewardvideo.events.REWARD', () => {
+		// 		this.exibirMensagem = false;
+		// 		this.sincronizeResultados();
+		// 	});
 
-			document.addEventListener('admob.rewardvideo.events.CLOSE', event => {
-				if(this.exibirMensagem) {
-					let alert = this.alertCtrl.create({
-						// title: 'Conclusão obrigatória do tempo promocional',
-						// message: `Para verificar se há novos sorteios para serem atualizados, certifique-se que o vídeo foi exibido completamente antes de fechar.`,
-						title: 'Exibição necessária',
-						message: `Desculpe, para verificar se há novos sorteios para serem atualizados, 
-							é necessário a exibição completa do vídeo.`,
-						enableBackdropDismiss: false,
-						buttons: [{
-							text: 'Ok',
-						}]
-					});
-					alert.present();
-				}
-				this.dismissObserver.next(true);
-				this.exibirMensagem = true;
-			});
+		// 	document.addEventListener('admob.rewardvideo.events.CLOSE', event => {
+		// 		if(this.exibirMensagem) {
+		// 			let alert = this.alertCtrl.create({
+		// 				// title: 'Conclusão obrigatória do tempo promocional',
+		// 				// message: `Para verificar se há novos sorteios para serem atualizados, certifique-se que o vídeo foi exibido completamente antes de fechar.`,
+		// 				title: 'Exibição necessária',
+		// 				message: `Desculpe, para verificar se há novos sorteios para serem atualizados, 
+		// 					é necessário a exibição completa do vídeo.`,
+		// 				enableBackdropDismiss: false,
+		// 				buttons: [{
+		// 					text: 'Ok',
+		// 				}]
+		// 			});
+		// 			alert.present();
+		// 		}
+		// 		this.dismissObserver.next(true);
+		// 		this.exibirMensagem = true;
+		// 	});
 
-			document.addEventListener('admob.rewardvideo.events.LOAD_FAIL', (event) => {
-				let alert = this.alertCtrl.create({
-					title: 'Erro ao buscar novos sorteios',
-					message: `Isso pode ter ocorrido por falha de conexão, verifique se está ativa a sua internet, 
-					se estiver e mesmo assim o erro persistir, tente mais tarde.
-						`,
-					enableBackdropDismiss: false,
-					buttons: [{
-						text: 'Ok',
-					}]
-				});
-				alert.present();
-				this.dismissObserver.next(true);
-			});
-		}
+		// 	document.addEventListener('admob.rewardvideo.events.LOAD_FAIL', (event) => {
+		// 		let alert = this.alertCtrl.create({
+		// 			title: 'Erro ao buscar novos sorteios',
+		// 			message: `Isso pode ter ocorrido por falha de conexão, verifique se está ativa a sua internet, 
+		// 			se estiver e mesmo assim o erro persistir, tente mais tarde.
+		// 				`,
+		// 			enableBackdropDismiss: false,
+		// 			buttons: [{
+		// 				text: 'Ok',
+		// 			}]
+		// 		});
+		// 		alert.present();
+		// 		this.dismissObserver.next(true);
+		// 	});
+		// }
 
 		this.dismissObserver = null;
 		this.dismiss = Observable.create(observer => {
@@ -73,32 +74,32 @@ export class RewardVideoAdMobService {
 	}
 	
     mostreAnuncioRewardVideo() {
-		if(this.plataforma.is('android')) {
-			let rewardVideoConfig: AdMobFreeRewardVideoConfig = {
-				isTesting: true, // Remove in production
-				autoShow: true,
-				// id: 'ca-app-pub-5335868077868255/8883684198'
-			};
+		// if(this.plataforma.is('android')) {
+		// 	let rewardVideoConfig: AdMobFreeRewardVideoConfig = {
+		// 		// isTesting: true, // Remove in production
+		// 		autoShow: true,
+		// 		id: 'ca-app-pub-5335868077868255/8883684198'
+		// 	};
 			
-			this.admob.rewardVideo.config(rewardVideoConfig);
+		// 	this.admob.rewardVideo.config(rewardVideoConfig);
 			
-			this.admob.rewardVideo.prepare().then(() => {
-			}).catch(e => {
-				let alert = this.alertCtrl.create({
-					title: 'Falha ao buscar novos resultados',
-					message: `Não foi possível buscar novos resultados de sorteios de loteria. Verifique sua conexão com a internet e tente novamente, 
-						caso essa mensagem seja exibida novamente, entre em contato com o suporte: sinaldasorteweb@gmail.com
-						`,
-					enableBackdropDismiss: false,
-					buttons: [{
-						text: 'Ok',
-					}]
-				});
-				alert.present();
-			});
-		} else {
+		// 	this.admob.rewardVideo.prepare().then(() => {
+		// 	}).catch(e => {
+		// 		let alert = this.alertCtrl.create({
+		// 			title: 'Falha ao buscar novos resultados',
+		// 			message: `Não foi possível buscar novos resultados de sorteios de loteria. Verifique sua conexão com a internet e tente novamente, 
+		// 				caso essa mensagem seja exibida novamente, entre em contato com o suporte: sinaldasorteweb@gmail.com
+		// 				`,
+		// 			enableBackdropDismiss: false,
+		// 			buttons: [{
+		// 				text: 'Ok',
+		// 			}]
+		// 		});
+		// 		alert.present();
+		// 	});
+		// } else {
 			this.sincronizeResultados();
-		}
+		// }
 	}
 
 	sincronizeResultados() {
